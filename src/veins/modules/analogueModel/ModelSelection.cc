149971955e602782829cab1bdbd97c1654d10f3e
Christian Nelson <christian.nelson@eit.lth.se>
2017-06-21 08:34:44 +0200

    Added the skeleton for the ModelSelection.

---
+++ src/veins/modules/analogueModel/ModelSelection.cc
@@ -0,0 +1,19 @@
+/*
+ * ModelSelection.cc
+ *
+ *  Created on: Jun 20, 2017
+ *      Author: skynet-user1
+ */
+
+#include "veins/modules/analogueModel/ModelSelection.h"
+#include "veins/base/messages/AirFrame_m.h"
+
+void ModelSelection::filterSignal(AirFrame *frame, const Coord& senderPos, const Coord& receiverPos) {
+    return;
+}
+
+double ModelSelectionMapping::getValue(const Argument& pos) const {
+    // TODO: Implement selection of LOS or OLOS model depending on situation.
+    return 1;
+}
+
---
+++ src/veins/modules/analogueModel/ModelSelection.h
@@ -0,0 +1,42 @@
+/*
+ * ModelSelection.h
+ *
+ *  Created on: Jun 20, 2017
+ *      Author: skynet-user1
+ */
+
+#ifndef SRC_VEINS_MODULES_ANALOGUEMODEL_MODELSELECTION_H_
+#define SRC_VEINS_MODULES_ANALOGUEMODEL_MODELSELECTION_H_
+
+#include "veins/base/phyLayer/AnalogueModel.h"
+#include "veins/base/phyLayer/MappingBase.h"
+#include "veins/base/modules/BaseWorldUtility.h"
+
+using Veins::AirFrame;
+
+class ModelSelectionMapping;
+
+class ModelSelection : public AnalogueModel {
+protected:
+    friend class ModelSelectionMapping;
+public:
+    ModelSelection();
+
+    virtual void filterSignal(AirFrame *frame, const Coord& sendersPos, const Coord& receiverPos);
+};
+
+class ModelSelectionMapping : public SimpleConstMapping {
+private:
+    ModelSelection* model;
+public:
+    ModelSelectionMapping(ModelSelection* model)
+        : SimpleConstMapping(DimensionSet::timeFreqDomain()), model(model) {}
+
+    virtual double getValue(const Argument& pos) const;
+
+    ConstMapping* constClone() const {
+        return new ModelSelectionMapping(*this);
+    }
+};
+
+#endif /* SRC_VEINS_MODULES_ANALOGUEMODEL_MODELSELECTION_H_ */
